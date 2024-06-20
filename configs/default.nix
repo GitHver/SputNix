1b39473b938c23a889efca6d73f85c5e4cb85833
er ég? <165333014+GitHver@users.noreply.github.com>
2024-06-20 02:45:16 +0000

    Update default.nix

--- configs/default.nix
+++ configs/default.nix
@@ -1,82 +1,91 @@
 { pkgs, ... }:
 
-  let
-    hostname = import ./../hostname.nix;
-  in
+let
+  hostname = import ./../hostname.nix;
+  # some-variable = "some_value";
+in
 
 { ###### Variable scope ########################################################
 
 # ====== Module imports ====================================================== #
   imports = [
-    ./../archive/${hostname}/hardware-configuration.nix
-    ./../archive/${hostname}/extra-hardware.nix
-    ./../modules/userlogicT1.nix
-    ./../modules/userlogicT2.nix
-    ./../modules/localization.nix
-    ./../modules/pipewire.nix
-    ./../modules/utilities.nix
-    ./../modules/gnome.nix
-    # ./../modules/steam.nix # if you want steam, uncomment this
+  ./../archive/${hostname}/hardware-configuration.nix
+  ./../archive/${hostname}/extra-hardware.nix
+  ./../modules/userlogic.nix    # Imports the user handling modules
+  ./../modules/essentials.nix   # Imports essential modules
+  ./../modules/gnome.nix        # the most popular desktop environment
+  # ./../modules/steam.nix      # if you want steam remote play, uncomment this
   ];
-		
+
   config = { ### Config scope ##################################################
 
-# ====== Bootloader ========================================================== #
+# ====== Bootloader ========================================================= #
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   /* Be VERY careful when changing this, nix is unbreakable in everything
   except 2 things: messing with user paths and messing with the boot-loader.
   So make sure you know what you are doing when rebuilding any changes here.
-  Best to first use a virtual machine or a "throw-away" computer */
+  Best to first use a virtual machine or a "throw-away" computer. */
 
 # ====== Network config ====================================================== #
   networking = {
-    hostName = "${hostname}";		# The name of your computer
-    networkmanager.enable = true;
-    #wireless.enable = true;		# comes with most DEs
-    firewall = {
-      enable = true;
-      #allowedTCPPorts = [ ... ];
-      #allowedUDPPorts = [ ... ];
+    hostName = "${hostname}";     # The name of your computer.
+    networkmanager.enable = true; # Networkmanager handles wifi and ethernet.
+    #wireless.enable = true;      # Unneccesary, Comes packaged with most DEs.
+    firewall = {                    # If you're having trouble with connection
+      enable = true;                # permissions, you can disable the firewall
+      #allowedTCPPorts = [ ... ];   # or open some ports here
+      #allowedUDPPorts = [ ... ];   # or here.
   };};
   services.openssh.enable = false;
 
 # ====== Localization ======================================================== #
   time.timeZone = "Europe/London";
-  locale-all = "en_GB.UTF8";
-  console.keyMap = "uk";		# Configure console keymap
-  services.xserver.xkb = {        # Configure keymap in X11
+  locale-all = "en_GB.UTF-8";       # default is "en_GB.UTF-8".
+  console.keyMap = "uk";            # Sets the console keymap.
+  services.xserver.xkb = {          # Set the keymap for Xserver.
     layout = "gb";
     variant = "";
   };
 
 # ====== Nix specific settings =============================================== #
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [];
-  users.mutableUsers = true;
+  programs.nix-ld.enable = true;              # Nix-ld is mostly for developers.
+  programs.nix-ld.libraries = with pkgs; [];  # doesn't hurt to have it though!
   nix.settings = {
-    allowed-users = [ "@wheel" ];
+    allowed-users = [ "@wheel" ];       # Note: the wheel group is for admins.
     experimental-features = [ "flakes" "nix-command" ];
   };
 
 # ====== Miscellaneous ======================================================= #
-  services.printing.enable = true;
-  xdg.portal.enable = true;
+  services.printing.enable = true;  # Printer protocols
+  xdg.portal.enable = true;         # X Screen portal
+  system.stateVersion = "24.05";    # What version of Nix to use
+
+# ====== User management ===================================================== #
+  users.mutableUsers = true;             # Makes the home directory writeable.
+  users.users = {                        # See *Users* for more info
+    "your-user-directory-name" = {         # example: "john-smith"
+    description  = "Your Display Name";    # example: "John Smith"
+    isNormalUser = true;
+    extragroups = [ "wheel" "networkmanager" ];
+  };};
 
 # ====== System packages ===================================================== #
-  system.stateVersion = "24.05";
   nixpkgs.config.allowUnfree = true;
-  services.flatpak.enable = true;
+  services.flatpak.enable = true;       # See "flatpaks" for more info.
+  # Below is where all the sytem-wide packages are installed.
+  # go to https://search.nixos.org/packages to search for programs.
   environment.systemPackages = with pkgs; [
+    # name-of-package
   # ==== pkgs ======================== #
-    nerdfonts
-    git
-    wl-clipboard
+    nerdfonts     # Font icon package.
+    wl-clipboard  # Wayland Clipboard tool.
   # ==== Terminal ==================== #
-    micro
-    lf
+    micro     # A mininal but caplable terminal text editor.
+    lf        # A light weight terminal file explorer.
   # ==== Miscellaneous =============== #
-    firefox
+    firefox   # This way you still have a browser while setting up home-manager
+    git       # Best learn to use git. it *WILL* make your life easier.
   ];
 
 };} #### End of variable & config scope. #######################################
